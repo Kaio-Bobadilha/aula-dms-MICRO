190769b7cac25bf13bde34c129ebfb438894c2f1
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2026-04-14 21:15:38 -0300

    feat: especializa banco de dados no microserviço de matrícula

---
+++ src/shared/infra/database/drizzle/0002_noisy_speedball.sql
@@ -0,0 +1,7 @@
+DROP TABLE "students" CASCADE;--> statement-breakpoint
+DROP TABLE "subjects" CASCADE;--> statement-breakpoint
+DROP TABLE "teachers" CASCADE;--> statement-breakpoint
+DROP TABLE "attendances" CASCADE;--> statement-breakpoint
+DROP TABLE "class_offerings" CASCADE;--> statement-breakpoint
+DROP TYPE "public"."attendance_status";--> statement-breakpoint
+DROP TYPE "public"."class_offering_status";
